3653a03ac03f17ed03389bf2a60dfef6c74607ef
= <=>
2024-09-04 08:53:43 +0530

    Get requests added

--- backend/Controllers/productController.js
+++ backend/Controllers/productController.js
@@ -1,20 +1,33 @@
-// get all products 
-//get one product
+const product = require('../Models/Product')
+
 // add product
 // delete product
 //update product
 
 const getAllProducts = async (req, res) => {
     try {
-        
+        //find() -> return all entities
+        const products = await product.find();
+        return res.status(200).json(product)
     }
-    catch(err) {
+    catch (err) {
         console.log(err);
+        return res.status(500).json({ message: "Failed to retrieve products." })
     }
 }
 
-const sayHi = async (req, res) => {
-    res.send("sucess")
+const getOneProduct = async (req, res) => {
+    try {
+        const { productID } = req.params
+        const findProduct = await product.findOne({ _id: productID })
+        if (!findProduct) return res.status(404).json({ message: "Product not found" })
+        return res.status(200).json(findProduct)
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Failed to retrieve product." })
+    }
 }
+
 
 module.exports = sayHi
