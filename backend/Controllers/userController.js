4c4348866618d912607c88d2eb6603480f5da0bb
= <=>
2024-09-12 08:57:13 +0530

    Controllers for only login

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -1,3 +1,17 @@
 const user = require('../Models/User');
 
-const 
+const Login = async (req, res) => {
+    try {
+        const { username, password } = req.body
+        const findUsername = await user.findOne({ userName: username })
+        if (!findUsername) return res.status(404).json({ message: "User doesn't exist" })
+        if (findUsername.password != password) {
+            return res.status(401).json({ message: "Credentials doesn't match" })
+        }
+        return res.status(200).json({ message: "Login Successful!" })
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Login Failed!" })
+    }
+}
